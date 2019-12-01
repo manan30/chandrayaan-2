9ddffb734d035ad00a238c8ce21e83c16c7e1c82
Manan Joshi <mananjoshi1995@gmail.com>
2019-12-01 16:59:58 -0500

    Updates

---
+++ precache-manifest.97c6dc7aa0f385bc00b5249ac881ba4e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660891b8d390cbf7be9e68e008953511",
+    "url": "/chandrayaan-2/index.html"
+  },
+  {
+    "revision": "a57a20e100c1e53617a2",
+    "url": "/chandrayaan-2/static/js/2.2eb7a178.chunk.js"
+  },
+  {
+    "revision": "2c6a120651639b5629ec",
+    "url": "/chandrayaan-2/static/js/main.c6fb349e.chunk.js"
+  },
+  {
+    "revision": "29619290ef1f102e10ac",
+    "url": "/chandrayaan-2/static/js/runtime-main.66304fb8.js"
+  },
+  {
+    "revision": "45ae6884f17efcd6ad13d7071f3bb2b8",
+    "url": "/chandrayaan-2/static/media/earth.45ae6884.jpg"
+  },
+  {
+    "revision": "c147e262013b259c840c2a72c8cf18d4",
+    "url": "/chandrayaan-2/static/media/jupiter.c147e262.jpg"
+  },
+  {
+    "revision": "b1404cfbacc5e3e4c08fd7ae1472d807",
+    "url": "/chandrayaan-2/static/media/mars.b1404cfb.jpg"
+  },
+  {
+    "revision": "05a75c74da22211353a8dd64c4ad6172",
+    "url": "/chandrayaan-2/static/media/mercury.05a75c74.jpg"
+  },
+  {
+    "revision": "17c1bff0c64a6a511e7ae707b660bf0f",
+    "url": "/chandrayaan-2/static/media/neptune.17c1bff0.jpg"
+  },
+  {
+    "revision": "278f00b03257d7d35c7701f85d2d38e4",
+    "url": "/chandrayaan-2/static/media/saturn.278f00b0.jpg"
+  },
+  {
+    "revision": "09a52beab6f825b7dcd20835e7f95b19",
+    "url": "/chandrayaan-2/static/media/sun.09a52bea.jpg"
+  },
+  {
+    "revision": "0465fbad731bbca3d6c88d36ee524a1c",
+    "url": "/chandrayaan-2/static/media/uranus.0465fbad.jpg"
+  },
+  {
+    "revision": "814a29210ee5cf9a88ed2ec6417baaa0",
+    "url": "/chandrayaan-2/static/media/venus.814a2921.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/chandrayaan-2/precache-manifest.97c6dc7aa0f385bc00b5249ac881ba4e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chandrayaan-2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
